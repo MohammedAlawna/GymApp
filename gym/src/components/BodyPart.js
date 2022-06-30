a405690f4f01de503e8ed0f9677783226feb9e53
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-06-30 03:32:13 +0100

    BodyPartSliderDone.

--- gym/src/components/BodyPart.js
+++ gym/src/components/BodyPart.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
 
-function BodyPart() {
-  return <div>BodyPart</div>;
-}
+const BodyPart = (item, setBodyPart, bodyPart) => {
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
+        backgroundColor: "white",
+        borderBottomLeftRadius: "15px",
+        width: "260px",
+        height: "270px",
+        cursor: "pointer",
+        gap: "44px",
+      }}
+      onClick={() => {
+        setBodyPart(item);
+      }}
+    >
+      <img src={Icon} alt="dumb" style={{ width: "40px", height: "40px" }} />
+      <Typography
+        fontSize="23px"
+        color="#3a1212"
+        textTransform="capitalize"
+        fontWeight="bold"
+      >
+        {item}
+      </Typography>
+    </Stack>
+  );
+};
 
 export default BodyPart;
