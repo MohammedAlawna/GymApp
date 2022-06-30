d2a0d66377f6748b7d914deb21a6c07e0d3eec3a
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-06-30 16:33:49 +0100

    Footeradded

--- gym/src/components/Footer.js
+++ gym/src/components/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/Logo-1.png";
 
-function Footer() {
-  return <div>Footer</div>;
-}
-
+const Footer = () => {
+  return (
+    <Box mt="80px" bgcolor="gold">
+      <Stack gap="35px" alignItems="center" px="38px" pt="24px">
+        <img src={Logo} width="200px" height="45px" />
+        <Typography variant="h5" pb="35px" mt="20px">
+          Developed by Mohammed Alawna.
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
 export default Footer;
