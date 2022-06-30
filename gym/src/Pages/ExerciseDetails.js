f086e134912236bef6cddcbe20abadfe0d49463e
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-06-30 15:28:49 +0100

    detailsPartAdded

---
+++ gym/src/Pages/ExerciseDetails.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import Details from "../components/Details";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+
+const ExerciseDetails = () => {
+  const [exerciseDetail, setExerciseDetails] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com/video/related";
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetails(exerciseDetailData);
+    };
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
+};
+export default ExerciseDetails;
